c30d613534797469710a04db043fadb8f5f6aa77
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2026-05-15 15:29:59 +0300

    Add error and loading components for improved user experience; refactor Home component to fetch and display country data asynchronously.

---
+++ frontend/src/app/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loading() {
+  return <main className="mx-auto w-full max-w-7xl px-4">loading</main>;
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,12 @@
-"use client";
+import { getCountries } from "@/lib/api/countries";
 
-import { SearchInput } from "@/components/ui/search-input";
-import { useDebounce } from "@/hooks/useDebounce";
-import { useState } from "react";
-
-export default function Home() {
-  const [search, setSearch] = useState("");
-  const debouncedSearch = useDebounce(search);
-
-  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
+export default async function Home() {
+  const countries = await getCountries({});
 
   return (
     <main className="mx-auto w-full max-w-7xl px-4">
-      <SearchInput value={search} onChange={handleSearch} />
+      {/* <SearchInput value={search} onChange={handleSearch} /> */}
+      <pre>{JSON.stringify(countries, null, 2)}</pre>
     </main>
   );
 }
